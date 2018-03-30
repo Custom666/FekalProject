d062840bfe09805aefe9f621da2db49a93d7e1a5
custom666 <32340961+custom666@users.noreply.github.com>
2018-03-30 15:50:32 +0200

    Initialization

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float Speed = 10f;
+
+        private Rigidbody _rigidbody;
+
+        private Vector3 _move;
+
+        // Use this for initialization
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            _move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * Speed;
+        }
+
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            _rigidbody.MovePosition(transform.position + _move);
+            _rigidbody.AddTorque(_move);
+        }
+    }
+}
